2f8ade839bcb2c99b1b47aa4718305c83609f005
Edimar <edimarjr51@gmail.com>
2026-06-27 23:15:35 -0300

    feat: add CookieService

---
+++ backend/backend/src/main/java/com/novaStack/backend/service/CookieService.java
@@ -0,0 +1,11 @@
+package com.novaStack.backend.service;
+
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CookieService {
+
+}
