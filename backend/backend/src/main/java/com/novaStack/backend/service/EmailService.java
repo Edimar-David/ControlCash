ca8a74a6dbc4cd73d3e79a308f02d8ab6f17e539
Edimar <edimarjr51@gmail.com>
2026-06-28 17:19:30 -0300

    feat: implement sendHtmlEmail in EmailService

--- backend/backend/src/main/java/com/novaStack/backend/service/EmailService.java
+++ backend/backend/src/main/java/com/novaStack/backend/service/EmailService.java
@@ -1,7 +1,32 @@
 package com.novaStack.backend.service;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService {
+    private final JavaMailSender mailSender;
+
+    public EmailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void sendHtmlEmail(String toEmail, String subject, String html)
+            throws MessagingException {
+
+        MimeMessage message = mailSender.createMimeMessage();
+
+        MimeMessageHelper helper =
+                new MimeMessageHelper(message, true, "UTF-8");
+
+        helper.setTo(toEmail);
+        helper.setSubject(subject);
+
+        helper.setText(html, true);
+
+        mailSender.send(message);
+    }
 }
