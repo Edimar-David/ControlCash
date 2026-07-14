a45d2cb9e1004814458abf1ff13a4e81882da07b
Edimar <edimarjr51@gmail.com>
2026-07-14 18:32:43 -0300

    feat: create OtpController

---
+++ backend/backend/src/main/java/com/novaStack/backend/controller/OtpController.java
@@ -0,0 +1,9 @@
+package com.novaStack.backend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/otp")
+public class OtpController {
+}
