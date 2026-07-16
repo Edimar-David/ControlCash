49a6c05e1b24c79acd7cf58670d8b39d85bb7282
Edimar <edimarjr51@gmail.com>
2026-07-16 19:54:34 -0300

    feat: create ResponseError record

---
+++ backend/backend/src/main/java/com/novaStack/backend/infra/exception/ResponseError.java
@@ -0,0 +1,12 @@
+package com.novaStack.backend.infra.exception;
+
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+
+public record ResponseError(
+        String message,
+        HttpStatus status,
+        LocalDateTime timestamp
+) {
+}
