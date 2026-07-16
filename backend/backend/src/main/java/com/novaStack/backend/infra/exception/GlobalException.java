f865edbb17c5804b5d1f52c2f7fba97cd0e44c2a
Edimar <edimarjr51@gmail.com>
2026-07-16 19:57:01 -0300

    feat: add global exception handling

---
+++ backend/backend/src/main/java/com/novaStack/backend/infra/exception/GlobalException.java
@@ -0,0 +1,25 @@
+package com.novaStack.backend.infra.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class GlobalException {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ResponseError> catchGlobalException(Exception ex){
+        System.out.println(ex.getMessage());
+        ResponseError response = new ResponseError(
+                "An unexpected error occurred",
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                LocalDateTime.now()
+        );
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+    }
+
+}
+
