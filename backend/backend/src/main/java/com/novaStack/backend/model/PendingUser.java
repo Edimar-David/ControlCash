dec608a80f8a624af2952b46ee0b925bf5345750
Edimar <edimarjr51@gmail.com>
2026-07-16 20:19:58 -0300

    feat: implement PendingUser attributes and methods

--- backend/backend/src/main/java/com/novaStack/backend/model/PendingUser.java
+++ backend/backend/src/main/java/com/novaStack/backend/model/PendingUser.java
@@ -1,4 +1,59 @@
 package com.novaStack.backend.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class PendingUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    @Column(nullable = false)
+    String name;
+    @Column(nullable = false, unique = true)
+    String email;
+    @Column(nullable = false)
+    String password;
+
+    public PendingUser(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public PendingUser() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
