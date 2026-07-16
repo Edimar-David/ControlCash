c928ce1770f28b3a67ce30ca22bea6014e2873e5
Edimar <edimarjr51@gmail.com>
2026-07-16 20:46:47 -0300

    fix: add @Repository annotation to PendingUserRepository

--- backend/backend/src/main/java/com/novaStack/backend/repository/PendingUserRepository.java
+++ backend/backend/src/main/java/com/novaStack/backend/repository/PendingUserRepository.java
@@ -2,6 +2,8 @@ package com.novaStack.backend.repository;
 
 import com.novaStack.backend.model.PendingUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PendingUserRepository extends JpaRepository<PendingUser, Long> {
 }
